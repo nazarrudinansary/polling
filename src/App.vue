9a5828e48afe8ec039e96f2f85be4a5449c44981
naz-red <nazarrudinansary@gmail.com>
2021-08-12 01:26:11 +0700

    mulai membuat sistem polling

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <!-- <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div> -->
+  <router-view />
+</template>
+
+<style>
+body{
+  background: rgb(116, 92, 255);
+  color: #fff;
+  overflow: hidden;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">
+    <section class="poll">
+      <h3 class="give-poll">
+        Silahkan berikan penilaian anda terhada pelayanan kami
+      </h3>
+
+      <div class="emoticon">
+        <p>//ini akan menjadi icon untuk polling</p>
+      </div>
+    </section>
+
+    <section class="finish">
+      <h4>Terima kasih telah memberikan penilaian terhadap kami</h4>
+    </section>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+  },
+};
+</script>
+
+<style type="text/css">
+  .container{
+    display: flex;
+    flex-wrap: wrap;
+    height: 100vh;
+    align-items: center;
+  }
+
+  section{
+    width: 100%;
+  }
+</style>
